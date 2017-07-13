75973c300e69dc348c7fff9a10d23f97bbcfffee
amacharla <noop2016@gmail.com>
2017-07-13 06:17:41 +0000

    _strtok done

---
+++ sandbox/getlineStrok.c
@@ -0,0 +1,25 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * tested getline and strtok
+ */
+int main(void)
+{
+	char *token, *buffer = NULL;
+	ssize_t j;
+	size_t len = 0;
+
+	/*stores stream in buffer, and its length*/
+	j = getline(&buffer, &len, stdin);
+
+	/*take first argument and space as delimiter*/
+	token = strtok(buffer, " ");
+
+	while(token != NULL) /*strtok returns NULL if no tokens left*/
+	{
+		printf(" %s\n", token);
+		token = strtok(NULL, " ");/*global pointer points to next word address*/
+	}
+	return (0);
+}
